24af792c96dc3a07a3bbbb302f04aecd92b0331f
ticoniq_ <okolietochi@gmail>
2024-05-15 03:50:33 +0100

    chore: Update page layout to include new component for page title and remote job filtering

---
+++ src/app/jobs/new/NewJobForm.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import H1 from "@/components/ui/h1";
+import { CreateJobValues, createJobSchema } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import Select from "@/components/ui/select";
+import { jobTypes, locationTypes } from "@/lib/job-types";
+
+export default function Page() {
+  const form = useForm<CreateJobValues>({
+    resolver: zodResolver(createJobSchema),
+  });
+
+  const {
+    handleSubmit,
+    watch,
+    trigger,
+    control,
+    setValue,
+    setFocus,
+    formState: { isSubmitting },
+  } = form;
+
+  async function onSubmit(values: CreateJobValues) {
+    alert(JSON.stringify(values, null, 2));
+  }
+
+  return (
+    <main className="max-w-3xl m-auto my-10 space-y-10 p-6">
+      <div className="space-y-5 text-center">
+        <H1>Find your perfect Developer</H1>
+        <p className="text-muted-foreground">
+          Get your job in front of the best developers in the Next.js community.
+        </p>
+      </div>
+      <div className="space-y-6 border rounded-lg p-4">
+        <div>
+          <h2 className="font-semibold">Job Details</h2>
+          <p className="text-muted-foreground">
+            Provide a job description and details
+          </p>
+        </div>
+        <Form {...form}>
+          <form className="space-y-4" noValidate onSubmit={handleSubmit(onSubmit)}>
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. Frontend Developer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job type</FormLabel>
+                  <FormControl>
+                    <Select {...field} defaultValue="">
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {jobTypes.map((jobtype) => (
+                        <option key={jobtype} value={jobtype}>
+                          {jobtype}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="companyLogo"
+              render={({ field: { value, ...fieldValues } }) => (
+                <FormItem>
+                  <FormLabel>Company Logo</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="locationType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Select {...field} defaultValue="">
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {locationTypes.map((locationType) => (
+                        <option key={locationType} value={locationType}>
+                          {locationType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+    </main>
+  )
+}
